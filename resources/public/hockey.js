d07ed7447af8fb4f3c26fa749faf2b4b6a696670
rob <rippinrobr@hotmail.com>
2012-09-04 11:07:25 -0400

    removed unnecessary reference to text field

--- resources/public/hockey.js
+++ resources/public/hockey.js
@@ -25693,7 +25693,6 @@ goog.require("cljs.core");
 goog.require("clojure.browser.event");
 goog.require("domina");
 cljs_intro.search.search_button = domina.by_id.call(null, "search-btn");
-cljs_intro.search.lname_field = domina.by_id.call(null, "lname");
 clojure.browser.event.listen.call(null, cljs_intro.search.search_button, "click", function() {
   return alert(domina.value.call(null, domina.by_id.call(null, "lname")))
 });
