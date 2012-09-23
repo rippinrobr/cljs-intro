2b46d2e9802d72c180df67d33ca65ec1a4c02569
rob <rippinrobr@hotmail.com>
2012-09-22 20:05:56 -0400

    added a version of the project.clj file for lein 2 as Michael Peterson

---
+++ lein-2-project.clj
@@ -0,0 +1,22 @@
+(defproject cljs-intro "1.0.0-SNAPSHOT"
+      :description "FIXME: write description"
+      :dependencies [[org.clojure/clojure "1.4.0"]
+                     [domina "1.0.0"]
+                     [hiccups "0.1.1"]
+                     [org.clojure/java.jdbc "0.2.3"]
+                     [sqlitejdbc "0.5.6"]
+                     [noir "1.3.0-beta10"]]
+      :plugins [[lein-cljsbuild "0.2.7"]]
+      :source-paths ["src/clj"]
+      :cljsbuild {
+                     :builds [{
+                        :source-path "src/cljs"
+                        :compiler {
+                             :output-to "resources/public/hockey.js"
+                             :optimization :whitespace
+                             :pretty-print true
+                             }
+                        }]
+                  }
+      :main cljs-intro.core
+    )
